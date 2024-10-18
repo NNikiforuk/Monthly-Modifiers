ef6b1a59869976d4d53fe125caa39d9caa3beff2
Jakub Trąd <jakubtrad@gmail.com>
2024-10-19 01:12:59 +0200

    Some changes (#1)

--- src/scripts/getCurrentChallenges.js
+++ src/scripts/getCurrentChallenges.js
@@ -1,6 +1,8 @@
 import { challenges } from "../data/challenges";
 import { XORShift } from "random-seedable";
 
+const MAX_CHALLENGES = 3;
+
 export async function getCurrentChallenges() {
 	const url = `https://beacon.nist.gov/beacon/2.0/pulse/last`;
 
@@ -13,7 +15,7 @@ export async function getCurrentChallenges() {
 
 		const seed = BigInt("0x" + randomValue);
 		const randomNumberGenerator = new XORShift(seed);
-		let numberOfChallenges = 1 + (randomNumberGenerator.int() % 3);
+		let numberOfChallenges = 1 + (randomNumberGenerator.int() % MAX_CHALLENGES);
 
 		let currentChallengesList = [...challenges];
 		let newChallenges = [];
@@ -21,7 +23,16 @@ export async function getCurrentChallenges() {
 		for (let i = 0; i < numberOfChallenges; i++) {
 			let challenge =
 				randomNumberGenerator.int() % currentChallengesList.length;
+
 			const newChallenge = currentChallengesList.splice(challenge, 1)[0];
+
+			if (newChallenge.collidesWith) {
+				newChallenge.collidesWith.forEach((slug) => {
+					newChallenges = newChallenges.filter(
+						(challenge) => challenge.slug !== slug
+					);
+				});
+			}
 
 			newChallenges.push(newChallenge);
 		}
